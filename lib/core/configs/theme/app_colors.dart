4027966f7448dc6ffdf2355739819442e3f1cbf0
mohamedjamaal <mohamed_gamal19@hotmail.com>
2025-01-21 11:01:10 +0200

    initialize app & splash screen

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xFFE91E63);
+  static const background = Color(0xff181A20);
+  static const scondBackground = Color(0xFF35383F);
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/presentation/splash/pages/splash.dart';
+
+void main() {
+  runApp(const MoviesApp());
+}
+
+class MoviesApp extends StatelessWidget {
+  const MoviesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashPage(),
+    );
+  }
+}
